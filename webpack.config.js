49e34dbf6c6932778d5293db2190498ab8703935
mtakayuki <maeda.takayuki@jp.fujitsu.com>
2016-04-06 17:33:54 +0900

    Use webpack and eslint

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: __dirname + '/src/tutorial.js',
+  output: {
+    path: __dirname + '/public/scripts',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+};

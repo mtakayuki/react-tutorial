a1eb083ebec49507e393190bea26b5b057f536bc
mtakayuki <maeda.takayuki@jp.fujitsu.com>
2016-04-07 15:03:30 +0900

    tutorial 6 and 7

--- src/Comment.js
+++ src/Comment.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import marked from 'marked';
 
 class Comment extends React.Component {
+  rawMarkup() {
+    return marked(this.props.children.toString(), { sanitize: true });
+    // return { _html: rawMarkup };
+  }
+
   render() {
     return (
         <div className="comment">
           <h2 className="commentAuthor">
             {this.props.author}
           </h2>
-          {this.props.children}
+          <span dangerouslySetInnerHTML={{ __html: this.rawMarkup()}} />
         </div>
     );
   }
